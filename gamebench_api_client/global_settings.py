66a5c35ca92bcdfd350f1abe1b03dc31ded60e20
Kawika A. Brennan <kawika.a.brennan@bigfishgames.com>
2019-04-16 16:02:40 -0700

    Added a new method for getting the username and password in global settings.
    Returned values are intended to be used as the argument for Authenticator.

--- gamebench_api_client/global_settings.py
+++ gamebench_api_client/global_settings.py
@@ -3,8 +3,8 @@
 GAMEBENCH_CONFIG = {
     'url': 'https://api.production.gamebench.net',
     'api_version': '/v1',
-    'username': '',
-    'password': '',
+    'username': 'john.smith',
+    'password': 'password',
     'company_id': '',
 }
 
@@ -18,3 +18,14 @@ def set_api_endpoint():
 
     api_endpoint = GAMEBENCH_CONFIG['url'] + GAMEBENCH_CONFIG['api_version']
     return api_endpoint
+
+
+def get_username_and_password():
+    """ Takes the username and password from global variable GAMEBENCH_CONFIG and returns it as a dictionary.
+
+    :return: user_credentials: Dictionary of key username and value password from the global variable GAMEBENCH_CONFIG
+    """
+    user_credentials = dict()
+    user_credentials['username'] = GAMEBENCH_CONFIG['username']
+    user_credentials['password'] = GAMEBENCH_CONFIG['password']
+    return user_credentials
