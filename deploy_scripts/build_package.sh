f37f01dbe9c128d71101be6f142dfbcb6e837480
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-04-26 10:29:52 -0700

    Added build steps to build_package.sh Added publish_package.sh and stepts to install twine and upload to PyPi. Bumped version in .bumpversion.cfg, conf.py and setup.py. Manually deployed 0.1.2 to PyPi. Added package name to gamebench_api_client.__init__.py Fixed search and replace in .bumpversion.cfg Moved all dev only packages to [dev-packages] in Pipfile. Rebuilt lock file. Added packages to setup.py using setuptools.find_packages(). Added long_description = README.md in setup.py. Added long_description content type as markdown in setup.py. Removed invalid email value in setup.py Changed install_requires to work with all compatible minor revisions and patches.

--- deploy_scripts/build_package.sh
+++ deploy_scripts/build_package.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+python3 -m pip install --upgrade setuptools wheel;
+python3 setup.py sdist bdist_wheel;
+
+
 
 
 
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
 
+with open("README.md", "r") as file_handle:
+    long_description = file_handle.read()
+
 setuptools.setup(
     name='GameBenchAPI-PyClient-BigFish',
-    version='0.1.1',
+    version='0.1.2',
+    packages=setuptools.find_packages(),
     description='A GameBench API Library Client.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/bigfishgames/GameBenchAPI-PyClient',
     author='Big Fish Games, Inc.',
-    author_email='https://gitter.im/bigfishgames/GameBench-API-PyClient',
+    author_email='',
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License',
@@ -14,9 +20,7 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     install_requires=[
-        'requests-mock>=1.5.2',
-        'pandas>=0.24.2',
-        'coverage>=4.5.3',
+        'pandas~=0.24.2',
         'urllib3>=1.24.2,<1.25',
     ]
 )
