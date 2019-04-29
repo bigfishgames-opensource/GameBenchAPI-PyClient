e3ca12d723a1b4fe0dda9879657ac291e71a7a41
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-04-29 11:04:29 -0700

    Testing a ::patch:: commit. Created get_bump_part.sh that returns a string bump2version can use. Placeholder bump_version.sh to run bump2version. Only dry-run for now.

--- deploy_scripts/bump_version.sh
+++ deploy_scripts/bump_version.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-# Placeholder for WIP development.
-bump2version --dry-run --list --verbose minor
+
+BUMP_PART="$(./get_bump_part.sh)"
+
+function bump_or_exit(){
+    if  [[ ${BUMP_PART} != "no bump" ]]; then
+        cd ..
+        echo $(bump2version --dry-run --list --verbose "${BUMP_PART}")
+        return 0
+    else
+        echo "No version bump specified in commit."
+        return 1
+    fi
+}
+
+bump_or_exit
+
+exit
--- deploy_scripts/get_bump_part.sh
+++ deploy_scripts/get_bump_part.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/env bash
+
+function get_commit_message(){
+    echo "$(git log -1 --pretty=%B)"
+}
+
+
+function set_bump_part(){
+    if grep '::patch::' <<< "${COMMIT_MESSAGE}"; then
+        BUMP_PART="patch";
+    elif grep '::minor::' <<< "${COMMIT_MESSAGE}"; then
+        BUMP_PART="minor";
+    elif grep '::major::' <<< "${COMMIT_MESSAGE}"; then
+        BUMP_PART="major";
+    else
+        BUMP_PART="no bump";
+    fi
+}
+
+function return_bump_part(){
+    echo "${BUMP_PART}";
+}
+
+COMMIT_MESSAGE="$(get_commit_message)";
+set_bump_part;
+return_bump_part;
+
+
