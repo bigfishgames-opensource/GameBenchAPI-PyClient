67574e0883dc1860e8c4a941ac4965cc1147a314
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-02 14:44:00 -0700

    Bumped version to 0.1.3 Working bump, build, and publish scripts.

--- deploy_scripts/publish_package.sh
+++ deploy_scripts/publish_package.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd ..;
 python3 -m pip install --upgrade twine;
 python3 -m twine upload dist/*;
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Big Fish Games, Inc.'
 author = 'Big Fish Games, Inc.'
 
 # The short X.Y version
-version = '0.1.2'
+version = '0.1.3'
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
