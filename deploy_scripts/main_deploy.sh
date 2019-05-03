7cde10fbfc547e6979b181c8b5456ebf90664953
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-03 09:53:09 -0700

    Added a script to call the other scripts as needed by Travis. Added commit and tag to bump2version config. Added deploy config to .travis.yml

---
+++ deploy_scripts/main_deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./bump_version.sh;
+./build_package.sh;
+./publish_package.sh;
+
--- deploy_scripts/publish_package.sh
+++ deploy_scripts/publish_package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python3 -m pip install --upgrade twine;
-python3 -m twine upload dist/*;
+python3 -m twine upload --username=$PyPi_UN --password=$PyPi_PW dist/*;
