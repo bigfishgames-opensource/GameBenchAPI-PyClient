2ae911f40fc559412dbf0def40e736c8c0ca15b4
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-06 10:47:57 -0700

    Updated paths to assume build and deploy scripts called from project root.

--- deploy_scripts/bump_version.sh
+++ deploy_scripts/bump_version.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-BUMP_PART="$(./get_bump_part.sh)"
+BUMP_PART="$(./deploy_scripts/get_bump_part.sh)"
 
 function bump_or_exit(){
     if  [[ ${BUMP_PART} != "no bump" ]]; then
         cd ..
-        echo "$(bump2version --list --verbose ${BUMP_PART})"
+        echo "$(bump2version --dry-run --list --verbose ${BUMP_PART})"
         return 0
     else
         echo "No version bump specified in commit."
