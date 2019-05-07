c6f04f794e171d200761150d04517695dfa7cd81
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-06 17:40:22 -0700

    ::patch:: Run bump2version from python3 -m.

--- deploy_scripts/bump_version.sh
+++ deploy_scripts/bump_version.sh
@@ -5,7 +5,7 @@ BUMP_PART="$(./deploy_scripts/get_bump_part.sh)"
 function bump_or_exit(){
     if  [[ ${BUMP_PART} != "no bump" ]]; then
         python3 -m pip install bump2version;
-        echo "$(python3 bump2version --list ${BUMP_PART})";
+        echo "$(python3 -m bump2version --list ${BUMP_PART})";
         return 0;
     else
         echo "No version bump specified in commit.";
