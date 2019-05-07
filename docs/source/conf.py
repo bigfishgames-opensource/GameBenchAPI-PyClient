1587ba2e890a18e83e471166a987291987d146b0
Joseph Losee <joseph.losee@bigfishgames.com>
2019-05-07 15:56:10 -0700

    Incremented the version to match the deployed version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Big Fish Games, Inc.'
 author = 'Big Fish Games, Inc.'
 
 # The short X.Y version
-version = '0.1.5'
+version = '0.1.6'
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
