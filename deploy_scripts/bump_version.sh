c95c9b6016cfee3f5b2db4425d86e3b1b966432e
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-06 17:17:23 -0700

    Removing --dry-run from bump_version.sh. Removing cd from bump_version.sh.

--- deploy_scripts/bump_version.sh
+++ deploy_scripts/bump_version.sh
@@ -4,8 +4,7 @@ BUMP_PART="$(./deploy_scripts/get_bump_part.sh)"
 
 function bump_or_exit(){
     if  [[ ${BUMP_PART} != "no bump" ]]; then
-        cd ..
-        echo "$(bump2version --dry-run --list --verbose ${BUMP_PART})"
+        echo "$(bump2version --list --verbose ${BUMP_PART})"
         return 0
     else
         echo "No version bump specified in commit."
