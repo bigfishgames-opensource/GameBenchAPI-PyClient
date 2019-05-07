9b5dd27afbb277f5f65931ed7467abb587c95861
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-06 17:37:35 -0700

    ::patch:: Adding pip install for bump2version. I might need to run bump2version from python3 -m.

--- deploy_scripts/bump_version.sh
+++ deploy_scripts/bump_version.sh
@@ -4,14 +4,16 @@ BUMP_PART="$(./deploy_scripts/get_bump_part.sh)"
 
 function bump_or_exit(){
     if  [[ ${BUMP_PART} != "no bump" ]]; then
-        echo "$(bump2version --list ${BUMP_PART})"
-        return 0
+        python3 -m pip install bump2version;
+        echo "$(python3 bump2version --list ${BUMP_PART})";
+        return 0;
     else
-        echo "No version bump specified in commit."
-        return 1
+        echo "No version bump specified in commit.";
+        return 1;
     fi
 }
 
 bump_or_exit
 
 exit
+
