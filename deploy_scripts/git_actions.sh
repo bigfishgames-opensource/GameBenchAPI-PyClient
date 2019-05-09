81205a3d4b062dd1ed31e85235183edc64884af4
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-09 09:21:36 -0700

    ::patch:: Fix for bug #58 After deployment, commit the bumped version and push to master without ci.

---
+++ deploy_scripts/git_actions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+git_config() {
+    git config --global user.email "travis@travis-ci.com";
+    git config --global user.name "Travis CI";
+}
+
+git_commit() {
+    git commit -m "[skip travis-ci]";
+}
+
+git_push() {
+    git remote rm origin;
+    git remote add origin https://"$3"@github.com/bigfishgames/GameBenchAPI-PyClient.git > /dev/null 2>&1
+    git push origin master --quiet;
+}
+
+git_config;
+git_commit;
+git_push;
