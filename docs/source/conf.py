3156a3509ff0f09f0a43083aeadede951d87517f
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-10 07:48:33 -0700

    Bump version: 0.1.8 → 0.1.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Big Fish Games, Inc.'
 author = 'Big Fish Games, Inc.'
 
 # The short X.Y version
-version = '0.1.8'
+version = '0.1.9'
 # The full version, including alpha/beta/rc tags
-release = '0.1.8'
+release = '0.1.9'
 
 
 # -- General configuration ---------------------------------------------------
