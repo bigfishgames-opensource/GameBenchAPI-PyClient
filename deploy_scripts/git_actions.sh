1f2c7a913a8008c064834151ada5c65b8c6c302a
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-10 09:05:49 -0700

    ::patch:: Fix for bug #58 Added git add .

--- deploy_scripts/git_actions.sh
+++ deploy_scripts/git_actions.sh
@@ -2,9 +2,8 @@
 
 git config --global user.email "travis@travis-ci.com";
 git config --global user.name "Travis CI";
-git commit -a -m "[skip travis-ci] version bumped!"
+git add .;
+git commit -m "[skip travis-ci] version bumped!";
 git remote rm origin;
 git remote add origin https://"$1"@github.com/bigfishgames/GameBenchAPI-PyClient.git > /dev/null 2>&1;
-git push origin master;
-
-
+git push origin master --quiet;
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2019, Big Fish Games, Inc.'
 author = 'Big Fish Games, Inc.'
 
 # The short X.Y version
-version = '0.1.11'
+version = '0.1.12'
 # The full version, including alpha/beta/rc tags
-release = '0.1.11'
+release = '0.1.12'
 
 
 # -- General configuration ---------------------------------------------------
