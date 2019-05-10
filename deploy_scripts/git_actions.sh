661a68361d62c887b7aa6b980bbd1592da7f3d61
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-10 10:36:03 -0700

    ::patch:: Fix for bug #58 Pushing a change to see why the push isn't happening.

--- deploy_scripts/git_actions.sh
+++ deploy_scripts/git_actions.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-git config --global user.email "travis@travis-ci.com";
-git config --global user.name "Travis CI";
-git add .;
-git commit --message "[skip travis-ci] version bumped!";
+git commit --all -m "[skip travis-ci] version bumped!";
 git remote rm origin;
 git remote add origin https://aaron-wilson-bfg:"$1"@github.com/bigfishgames/GameBenchAPI-PyClient.git;
 git push origin master;
