66e6dbdded33abed5aa05477449f947958d14c68
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-10 07:48:25 -0700

    ::patch:: Fix for bug #58 Adding git actions to commit and push. The commit message includes a skip ci directive that should work for Travis CI.

--- deploy_scripts/git_actions.sh
+++ deploy_scripts/git_actions.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git_config() {
-    git config --global user.email "travis@travis-ci.com";
-    git config --global user.name "Travis CI";
-}
+#git_config() {
+#    git config --global user.email "travis@travis-ci.com";
+#    git config --global user.name "Travis CI";
+#}
 
 git_commit() {
     git commit -m "[skip travis-ci] version bumped!";
@@ -11,10 +11,13 @@ git_commit() {
 
 git_push() {
     git remote rm origin;
-    git remote add origin https://"$3"@github.com/bigfishgames/GameBenchAPI-PyClient.git > /dev/null 2>&1
-    git push origin master --quiet;
+    git remote add origin https://"$3"@github.com/bigfishgames/GameBenchAPI-PyClient.git
+    git push origin master ; #--quiet
 }
 
-git_config;
+#git_config;
 git_commit;
 git_push;
+
+
+#> /dev/null 2>&1
