53fd25c4ff696244a7db6579b7acafd78f88bdc2
Aaron Wilson <49415734+aaron-wilson-bfg@users.noreply.github.com>
2019-05-13 11:35:39 -0700

    ::patch:: Fix for bug #58 Looks to be working now: https://travis-ci.com/bigfishgames/GameBenchAPI-PyClient/jobs/199780583 Adding back in quiet modes and redirecting output to dev/null.

--- deploy_scripts/git_actions.sh
+++ deploy_scripts/git_actions.sh
@@ -7,10 +7,10 @@ git stash;
 
 git checkout master;
 
-git stash pop;
+git stash pop --quiet;
 git commit --all -m "[skip travis-ci] version bumped!";
 
 git remote rm origin;
-git remote add origin https://aaron-wilson-bfg:"$1"@github.com/bigfishgames/GameBenchAPI-PyClient.git;
+git remote add origin https://aaron-wilson-bfg:"$1"@github.com/bigfishgames/GameBenchAPI-PyClient.git > /dev/null 2>&1;
 
-git push -u origin master;
+git push -u origin master --quiet;
