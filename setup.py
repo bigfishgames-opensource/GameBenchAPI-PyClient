d5becb4ea34cdf271e32f8302552ee1366715dad
Travis CI <travis@travis-ci.com>
2019-05-28 20:57:00 +0000

    [skip travis-ci] version bumped!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file_handle:
 
 setuptools.setup(
     name='GameBenchAPI-PyClient-BigFish',
-    version='0.1.20',
+    version='1.0.0',
     packages=setuptools.find_packages(),
     description='A GameBench API Client Library.',
     long_description=long_description,
